b7ad0e41b8828b780ebedf72edba0f272c1f7ffd
Andres Maldonado <pspdrew@hotmail.es>
2019-06-14 17:45:01 +0200

    SendtoPoolTimer begin

---
+++ SendToPoolTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// ANDRES
+
+public class SendToPoolTimer : MonoBehaviour, IPoolable
+{
+    public void OnPoolSpawn()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPoolUnSpawn()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
