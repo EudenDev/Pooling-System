52be097e6c65fab03de7d976ef33ccaf4fd467ed
Andres Maldonado <pspdrew@hotmail.es>
2019-06-14 17:53:54 +0200

    Send To Pool Component done

--- SendToPoolTimer.cs
+++ SendToPoolTimer.cs
@@ -2,28 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 // ANDRES
-
+/// <summary>
+/// Calls <see cref="Pooling.SendToPool(GameObject)"/> after a give duration.
+/// The duration is reset every new spawn.
+/// </summary>
 public class SendToPoolTimer : MonoBehaviour, IPoolable
 {
+    [Tooltip("Time in seconds before unspawning this object.")]
+    public float duration = 3f;
+    private float currentTime;
+
+    [Header("Options")]
+    [Tooltip("Use unscaled delta time in the timer.")]
+    public bool useUnscaledTime;
+
+    [Tooltip("If false this component only works on instances from Pooling." +
+        " Set this to true to run on object that are already in the scene" +
+        " or will be created with Instantiate().")]
+    public bool alwaysRun;
+    private bool usesPooling;
+
     public void OnPoolSpawn()
     {
-        throw new System.NotImplementedException();
+        usesPooling = true;
+        currentTime = duration;
     }
 
     public void OnPoolUnSpawn()
     {
-        throw new System.NotImplementedException();
+        // Nothing here ( ·_)·
     }
 
-    // Use this for initialization
     void Start()
     {
-
+        currentTime = duration;
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        if (!alwaysRun && !usesPooling)       
+            return;
+        // -
+        float dt = useUnscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
+        currentTime = Mathf.MoveTowards(currentTime, 0f, dt);
+        if (Mathf.Abs(currentTime) < float.Epsilon)
+        {
+            Pooling.SendToPool(gameObject);
+        }
     }
 }
